c18b7583c5ff3f8adaa1edf183af9483e01e8fde
Benjamin Beach <bmbeach@uncg.edu>
2022-09-14 15:06:31 -0400

    changed the code to use the movie api, removed everything relating to the university api and quotes api

--- src/main/java/com/csc340/RestAPI/RestApiController.java
+++ src/main/java/com/csc340/RestAPI/RestApiController.java
@@ -1,9 +1,5 @@
 package com.csc340.RestAPI;
 
-import java.util.Arrays;
-import java.util.Iterator;
-import java.util.List;
-import org.json.JSONArray;
 import org.json.JSONObject;
 
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,68 +8,41 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
 /**
- *
- * @author sentini
+ * CSC 340
+ * code is based on the provided example: https://github.com/sunnyntini/csc340-rest-api-demo
+ * @author benjamin beach
  */
 @RestController
 public class RestApiController {
 
-    @GetMapping("/hello")
+        @GetMapping("/hello")
     public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
         return String.format("Hello, %s!", name);
     }
 
     /**
-     * Get a random quote from quotable and make it available at this endpoint.
+     * get a random movie from the moviehut api and make it available at this endpoint.
      *
      * @return
      */
-    @GetMapping("/quote")
-    public Object getQuote() {
-        String url = "https://api.quotable.io/random";
+    @GetMapping("/movie")
+    public Object getMovie() {
+        String url = "https://k2maan-moviehut.herokuapp.com/api/random";
         RestTemplate restTemplate = new RestTemplate();
-        Object jSonQuote = restTemplate.getForObject(url, Object.class);
+        Object jSonMovie = restTemplate.getForObject(url, Object.class);
 
-        //Print the whole response to console.
-        String quote = restTemplate.getForObject(url, String.class);
-        //Parse out the most important info from the response.
-        JSONObject jo = new JSONObject(quote);
+        //print everything from the api into the console
+        String movie = restTemplate.getForObject(url, String.class);
+        //parse the most important parts and print them into the console
+        JSONObject jo = new JSONObject(movie);
         System.out.println(jo.toString());
-        String quoteAuthor = jo.getString("author");
-        String quoteContent = jo.getString("content");
-        System.out.println(quoteAuthor);
-        System.out.println(quoteContent);
+        String movieName = jo.getString("name");
+        String movieDirector = jo.getString("director");
+        String movieOverview = jo.getString("overview");
+        System.out.println(movieName + ", " + movieDirector);
+        System.out.println(movieOverview);
 
-        return jSonQuote;
-    }
-
-    /**
-     * Get a list of universities from hipolabs and parse the most important
-     * info to console.
-     *
-     * @return
-     */
-    @GetMapping("/universities")
-    public List<Object> getUniversities() {
-        String url = "http://universities.hipolabs.com/search?name=sports";
-        RestTemplate restTemplate = new RestTemplate();
-        Object[] universities = restTemplate.getForObject(url, Object[].class);
-
-        //Print the whole response to console
-        JSONArray ja = new JSONArray(universities);
-        System.out.println(ja.toString());
-
-        //Parse out relevant info from each entry in the response
-        Iterator<Object> iter = ja.iterator();
-        while (iter.hasNext()) {
-            JSONObject jo = (JSONObject) iter.next();
-            String name = jo.getString("name");
-            String country = jo.getString("country");
-            System.out.println(name + " : " + country);
-
-        }
-
-        return Arrays.asList(universities);
+        return jSonMovie;
     }
 
 }
